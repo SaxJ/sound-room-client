e01517f945b68735fab71a0bfb73e6c2f2db9da6
Saxon Jensen <saxon.jensen@healthengine.com.au>
2022-09-27 23:18:20 +0800

    Connect to a room

--- main.js
+++ main.js
@@ -1,44 +1,55 @@
 let socket;
+const roomInput = document.getElementById("room");
+
 function startSocket() {
-    socket = new WebSocket("wss://sound-room-server-production.up.railway.app");
+  const roomName = roomInput.getAttribute("value");
+  socket = new WebSocket(
+    `wss:sound-room-server-production.up.railway.app/${roomName}`
+  );
 
-    socket.addEventListener("open", () => {
-        document.getElementById("status").innerText = "connected";
-    });
+  socket.addEventListener("open", () => {
+    document.getElementById("status").innerText = `connected ${roomName}`;
+  });
 
-    socket.addEventListener("close", () => {
-        document.getElementById("status").innerText = "disconnected";
-        setTimeout(() => startSocket(), 1000);
-    });
+  socket.addEventListener("close", () => {
+    document.getElementById("status").innerText = "disconnected";
+    setTimeout(() => startSocket(), 1000);
+  });
 
-    socket.addEventListener("message", (event) => {
-        const msg = event.data;
-        const audioSource = new Audio(`${msg}.wav`);
-        audioSource.play();
-    });
+  socket.addEventListener("message", (event) => {
+    const msg = event.data;
+    const audioSource = new Audio(`${msg}.wav`);
+    audioSource.play();
+  });
 
-    socket.addEventListener("error", () => {
-        document.getElementById("status").innerText = "error";
-    });
+  socket.addEventListener("error", () => {
+    document.getElementById("status").innerText = "error";
+  });
 }
-startSocket();
 
 const context = new AudioContext();
 const eventTypes = [
-    "cheer",
-    "clap",
-    "cry",
-    "laugh",
-    "woof",
-    "quack",
-    "boo",
-    "wolf",
-    "drum",
-    "lame",
+  "cheer",
+  "clap",
+  "cry",
+  "laugh",
+  "woof",
+  "quack",
+  "boo",
+  "wolf",
+  "drum",
+  "lame",
 ];
 
 eventTypes.forEach((type) => {
-    document.getElementById(type).addEventListener("click", () => {
-        socket.send(type);
-    });
+  document.getElementById(type).addEventListener("click", () => {
+    socket.send(type);
+  });
 });
+
+document.getElementById("generate").addEventListener("click", () => {
+  id = crypto.randomUUID();
+  roomInput.setAttribute("value", id);
+});
+
+document.getElementById("connect").addEventListener("click", startSocket);
