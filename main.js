f1691c37bd876f6b6fe6c7964d4108b73d26d4a0
Saxon Jensen <saxon.jensen@healthengine.com.au>
2022-09-27 10:40:15 +0800

    Fuck

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
 let socket;
 function startSocket() {
     socket = new WebSocket("wss://sound-room-server-production.up.railway.app");
+
+    socket.addEventListener("open", () => {
+        document.getElementById("status").innerText = "connected";
+    });
+
+    socket.addEventListener("close", () => {
+        document.getElementById("status").innerText = "disconnected";
+        setTimeout(() => startSocket(), 1000);
+    });
+
+    socket.addEventListener("message", (event) => {
+        const msg = event.data;
+        const audioSource = new Audio(`${msg}.wav`);
+        audioSource.play();
+    });
+
+    socket.addEventListener("error", () => {
+        document.getElementById("status").innerText = "error";
+    });
 }
 startSocket();
 
@@ -22,23 +41,4 @@ eventTypes.forEach((type) => {
     document.getElementById(type).addEventListener("click", () => {
         socket.send(type);
     });
-});
-
-socket.addEventListener("open", () => {
-    document.getElementById("status").innerText = "connected";
-});
-
-socket.addEventListener("close", () => {
-    document.getElementById("status").innerText = "disconnected";
-    setTimeout(() => startSocket(), 1000);
-});
-
-socket.addEventListener("message", (event) => {
-    const msg = event.data;
-    const audioSource = new Audio(`${msg}.wav`);
-    audioSource.play();
-});
-
-socket.addEventListener("error", () => {
-    document.getElementById("status").innerText = "error";
 });
