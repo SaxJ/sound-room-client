6ffb14f64c4f5159f9dee6594076d2fdaed17529
Saxon Jensen <saxon.jensen@healthengine.com.au>
2022-09-28 21:19:40 +0800

    Better wording

--- main.js
+++ main.js
@@ -1,54 +1,53 @@
 const roomName = window.location.hash.substring(1);
 const eventTypes = [
-  "astonished",
-  "cheer",
-  "clap",
-  "cry",
-  "laugh",
-  "woof",
-  "quack",
-  "boo",
-  "wolf",
-  "drum",
-  "lame",
+    "astonished",
+    "cheer",
+    "clap",
+    "cry",
+    "laugh",
+    "woof",
+    "quack",
+    "boo",
+    "wolf",
+    "drum",
+    "lame",
 ];
 
 let socket;
 function startSocket() {
-  socket = new WebSocket(
-    //`ws://localhost:9160/${roomName}`
-    `wss://sound-room-server-production.up.railway.app/${roomName}`
-  );
+    socket = new WebSocket(
+        //`ws://localhost:9160/${roomName}`
+        `wss://sound-room-server-production.up.railway.app/${roomName}`
+    );
 
-  socket.addEventListener("open", () => {
-    document.getElementById("status").innerText = `connected ${roomName}`;
-  });
+    socket.addEventListener("open", () => {
+        document.getElementById("status").innerText = `connected ${roomName}`;
+    });
 
-  socket.addEventListener("close", () => {
-    document.getElementById("status").innerText = "disconnected";
-    setTimeout(() => startSocket(), 1000);
-  });
+    socket.addEventListener("close", () => {
+        document.getElementById("status").innerText = "disconnected";
+    });
 
-  socket.addEventListener("message", (event) => {
-    const msg = event.data;
-    const audioSource = new Audio(`${msg}.wav`);
-    audioSource.play();
-  });
+    socket.addEventListener("message", async (event) => {
+        const msg = await event.data.text();
+        const audioSource = new Audio(`${msg}.wav`);
+        audioSource.play();
+    });
 
-  socket.addEventListener("error", () => {
-    document.getElementById("status").innerText = "error";
-  });
+    socket.addEventListener("error", () => {
+        document.getElementById("status").innerText = "error";
+    });
 }
 startSocket();
 
 document.getElementById("generate").addEventListener("click", () => {
-  id = crypto.randomUUID();
-  window.location.href = `/#${id}`;
-  window.location.reload();
+    id = crypto.randomUUID();
+    window.location.href = `/#${id}`;
+    window.location.reload();
 });
 
 eventTypes.forEach((type) => {
-  document.getElementById(type).addEventListener("click", () => {
-    socket.send(type);
-  });
+    document.getElementById(type).addEventListener("click", () => {
+        socket.send(type);
+    });
 });
