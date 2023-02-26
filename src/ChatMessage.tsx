7f79d66a26497fb3a38392558151a33a3973d4e0
Mark <mark+github@tan.gl>
2023-02-26 11:59:59 +0800

    history has emoji instead of text, tighter css (#17)

--- src/ChatMessage.tsx
+++ src/ChatMessage.tsx
@@ -3,12 +3,35 @@ interface Props {
     avatar: string;
 };
 
+const emoji: { [key: string]: string } = {
+    astonished: "😲",
+    boo: "👎",
+    cheer: "🎉",
+    clap: "👏",
+    cry: "😢",
+    drum: "🥁",
+    airhorn: "📣",
+    punch: "👊",
+    fart: "💨",
+    lame: "🤡",
+    laugh: "😂",
+    rofl: "🤣",
+    lol: "😜",
+    santa: "🎅",
+    quack: "🦆",
+    wolf: "🐺",
+    woof: "🐶",
+    gong: "🟤",
+    shh: "🤫",
+    wow: "🤯",
+};
+
 export const ChatMessage = ({ message, avatar }: Props) => {
     const cmd = message.split(';')[0] ?? 'unknown';
 
     return (<>
         <div className="message">
-            <p>{cmd}</p>
+            <span className="emoji">{emoji[cmd]}</span>
             <img src={avatar} alt="Avatar" />
         </div>
     </>);
